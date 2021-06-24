92b3dc7304db346e7591a240b1e38354b1e676a5
dennis.chan <dennis.chan@mediatek.com>
2021-06-24 16:33:32 +0800

    [ALPS05759285] disp: Enhance 6382 dsc config
    [Detail] Enhance 6382 dsc config
    
    [Solution] Enhance 6382 dsc config
    (commit 8f775bd5f421 ("[ALPS05740252] disp: Enhance 6382 dsc config"))
    
    MTK-Commit-Id: 205ace5ce98ca619f08ad3e138348e2e7c633b21
    
    Change-Id: Icd3efc8cb66ae04d5a565030fcb53e405532a645
    Signed-off-by: Guo Li <guo.li@mediatek.com>
    CR-Id: ALPS05759285
    Feature: [Module]Clock Buffer

--- drivers/misc/mediatek/base/power/clkbuf_v1/mt6768/mtk_clkbuf_hw.c
+++ drivers/misc/mediatek/base/power/clkbuf_v1/mt6768/mtk_clkbuf_hw.c
@@ -785,7 +785,6 @@ void clk_buf_disp_ctrl(bool onoff)
 {
 	int pwrap_dcxo_en;
 
-	pr_info("%s:onoff=%d\n", __func__, onoff);
 	pwrap_dcxo_en = clkbuf_readl(DCXO_ENABLE) & ~DCXO_NFC_ENABLE;
 	clkbuf_writel(DCXO_ENABLE, pwrap_dcxo_en);
 	if (onoff) {
@@ -809,7 +808,6 @@ void clk_buf_disp_ctrl(bool onoff)
 			PMIC_XO_EXTBUF3_EN_M_SHIFT);
 		pmic_clk_buf_swctrl[XO_NFC] = 0;
 	}
-
 }
 EXPORT_SYMBOL(clk_buf_disp_ctrl);
 
@@ -1536,6 +1534,7 @@ short is_clkbuf_bringup(void)
 
 void clk_buf_post_init(void)
 {
+#ifndef CONFIG_MTK_MT6382_BDG
 #if defined(CONFIG_MTK_UFS_SUPPORT)
 	int boot_type;
 
@@ -1548,6 +1547,7 @@ void clk_buf_post_init(void)
 #else
 	clk_buf_ctrl_internal(CLK_BUF_UFS, CLK_BUF_FORCE_OFF);
 	CLK_BUF7_STATUS = CLOCK_BUFFER_DISABLE;
+#endif
 #endif
 
 //#ifndef CONFIG_NFC_CHIP_SUPPORT
