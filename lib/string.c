606d80f2439f7c10f668ef939f954f77cdf47d19
Vaisakh Murali <mvaisakh@statixos.com>
2022-03-03 01:02:39 +0600

    Revert "lib/string: Use strchr() in strpbrk()"
    This reverts commit e74e4efb8a5e2859813581cbf3cc2afb46d8b739.

--- lib/string.c
+++ lib/string.c
@@ -624,11 +624,13 @@ EXPORT_SYMBOL(strcspn);
  */
 char *strpbrk(const char *cs, const char *ct)
 {
-	const char *sc;
+	const char *sc1, *sc2;
 
-	for (sc = cs; *sc != '\0'; ++sc) {
-		if (strchr(ct, *sc))
-			return (char *)sc;
+	for (sc1 = cs; *sc1 != '\0'; ++sc1) {
+		for (sc2 = ct; *sc2 != '\0'; ++sc2) {
+			if (*sc1 == *sc2)
+				return (char *)sc1;
+		}
 	}
 	return NULL;
 }
