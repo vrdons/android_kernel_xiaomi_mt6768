8224a41fcf8486b77614e77a7098b21a5dcf6294
Vrdons <vrdons@proton.me>
2026-04-15 15:11:37 +0300

    sound: Import selene drivers

--- sound/soc/codecs/mt6358.c
+++ sound/soc/codecs/mt6358.c
@@ -1825,9 +1825,11 @@ static int mtk_hp_spk_enable(struct mt6358_priv *priv)
 	if (priv->apply_n12db_gain)
 		regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON7,
 				0xff, 0x0004);
-
+	// I really wanted to find the real definition, but i cloud not find it
 	/* Audio left headphone input multiplexor selection : LOL */
+	#ifndef CONFIG_TARGET_PRODUCT_SELENECOMMON
 	set_hp_l_input_mux(priv, HP_INPUT_MUX_LOL);
+	#endif
 
 	/* Disable headphone short-circuit protection */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON0,
@@ -1893,12 +1895,20 @@ static int mtk_hp_spk_enable(struct mt6358_priv *priv)
 	/* Set LOL gain to normal gain step by step */
 	regmap_write(priv->regmap, MT6358_ZCD_CON1, DL_GAIN_N_10DB_REG);
 
+	#ifndef CONFIG_TARGET_PRODUCT_SELENECOMMON
 	/* Switch HPL MUX to Line-out */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON0,
 			0x3 << 8, 0x01 << 8);
 	/* Switch HPR MUX to DAC-R */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON0,
 			0x3 << 10, 0x2 << 10);
+	#else
+	/* Switch HPL MUX to HS */
+	regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON0, 0x3300);
+	/* Switch HPR MUX to LOL */
+	regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON0, 0x3700);
+	#endif
+
 	/* Enable HP aux output stage */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON1,
 			0xff, 0x0c);
@@ -1973,6 +1983,14 @@ static int mtk_hp_spk_enable(struct mt6358_priv *priv)
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON7,
 			0x3 << 2, 0x2 << 2);
 
+	#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	/* Switch HS MUX to audio DAC */
+	regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON6, 0x009b);
+
+    //lch inverse
+	regmap_update_bits(priv->regmap, MT6358_AFUNC_AUD_CON0,
+		0x4000, 0x1<<10);
+	#endif
 	/* Disable Pull-down HPL/R to AVSS28_AUD */
 	hp_pull_down(priv, false);
 
@@ -2077,6 +2095,11 @@ static int mtk_hp_spk_disable(struct mt6358_priv *priv)
 	/* Set HP CMFB gate rstb */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON4,
 			0x1 << 6, 0x0);
+	#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+        /* Switch HS MUX to audio DAC */
+        regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON6, 0x0090);
+    #endif
+
 	/* disable Pull-down HPL/R to AVSS28_AUD */
 	hp_pull_down(priv, false);
 
@@ -2277,6 +2300,10 @@ static int mt_lo_event(struct snd_soc_dapm_widget *w,
 	struct snd_soc_component *cmpnt = snd_soc_dapm_to_component(w->dapm);
 	struct mt6358_priv *priv = snd_soc_component_get_drvdata(cmpnt);
 
+	#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	uint32_t reg_value = 0;
+	#endif
+
 	dev_dbg(priv->dev, "%s(), event 0x%x, mux %u\n",
 		 __func__,
 		 event,
@@ -2349,12 +2376,27 @@ static int mt_lo_event(struct snd_soc_dapm_widget *w,
 				   0x1, 0x1);
 
 		/* Enable Audio DAC  */
+		#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+		regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON0, 0x000F);
+		#else
 		regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON0, 0x0009);
+		#endif
+
 		/* Enable low-noise mode of DAC */
 		regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON9, 0x0001);
 	    /* Switch LOL MUX to audio DAC */
+		#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+		regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON7,
+				   0x3 << 2, 0x1 << 2);
+		//lch inverse
+		regmap_write(priv->regmap, MT6358_AFUNC_AUD_CON0,0xCFA1);
+		//add by mtk for debug
+		regmap_read(priv->regmap, MT6358_AFUNC_AUD_CON0, &reg_value);
+		dev_info(priv->dev, "%s(), MT6358_AFUNC_AUD_CON0 0x%x, \n",__func__,reg_value);
+		#else
 		regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON7,
 				   0x3 << 2, 0x2 << 2);
+		#endif
 
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
@@ -2483,7 +2525,11 @@ static int mt_rcv_event(struct snd_soc_dapm_widget *w,
 				   0x1, 0x1);
 
 		/* Enable Audio DAC  */
+		#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+		regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON0, 0x000F);
+		#else
 		regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON0, 0x0009);
+		#endif
 		/* Enable low-noise mode of DAC */
 		regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON9, 0x0001);
 		/* Switch HS MUX to audio DAC */
