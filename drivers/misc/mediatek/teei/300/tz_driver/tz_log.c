21f675ef47e44fa7161406c48557514287668cd2
Vrdons <vrdons@proton.me>
2026-04-16 15:25:09 +0300

    Reapply "teei: Import selene changes"
    Less memory :O
    
    This reverts commit c1100fbfe6bf71f5f56bf7af84eca032d375532d.

--- drivers/misc/mediatek/teei/300/tz_driver/tz_log.c
+++ drivers/misc/mediatek/teei/300/tz_driver/tz_log.c
@@ -221,9 +221,11 @@ int teei_log_fn(void *work)
 		retVal = wait_for_completion_interruptible(&teei_log_comp);
 		if (retVal != 0)
 			continue;
-		//spin_lock_irqsave(&s->lock, flags);
+#ifdef CONFIG_MICROTRUST_TZ_LOG
+		spin_lock_irqsave(&s->lock, flags);
 		tz_driver_dump_logs(s);
-		//spin_unlock_irqrestore(&s->lock, flags);
+		spin_unlock_irqrestore(&s->lock, flags);
+#endif
 	}
 
 	return NOTIFY_OK;
@@ -436,4 +438,3 @@ int tz_log_remove(struct platform_device *pdev)
 
 	return 0;
 }
-
